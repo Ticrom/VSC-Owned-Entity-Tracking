d3ed85674ab65ecade2f7277035c6623fb399dad
InternetofDisplays <Jessica.haypikingdom@gmail.com>
2026-03-28 23:24:38 -0400

    Make tracked types configurable and add commands
    Move hardcoded tracked entity types into TrackerConfig so server operators can customize which entities are tracked. Add /tracktp (teleport tracked entity to owner), /selftrack (list your tracked entities) and /trackreload (reload config at runtime) commands, and update command privileges to use Privilege.ban for several admin commands. Implement CmdTP, CmdSelfTrack and CmdReload handlers, update the periodic tick listener when reloading config, remove the static TrackedEntityTypes HashSet, and bump mod version to 1.2.0.

--- EntityTracking/TrackerConfig.cs
+++ EntityTracking/TrackerConfig.cs
@@ -1,8 +1,16 @@
+using System.Collections.Generic;
+
 namespace EntityTracking
 {
     public class TrackerConfig
     {
         public string TrackCommandPrivilege { get; set; } = "chat";
         public int UpdateIntervalSeconds { get; set; } = 300;
+        public List<string> TrackedEntityTypes { get; set; } = new List<string>
+        {
+            "sailboat", "boat", "raft", "canoe",
+            "wolf", "hyena", "aurochs", "moose", "bighorn",
+            "sawtooth", "tameddeer", "elk", "deer", "horse"
+        };
     }
 }
